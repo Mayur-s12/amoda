4675a80cc5e58fb801e12ff3622e74dfdbfa0458
mayur <mayursahare91@gmail.com>
2023-12-18 23:05:33 +0530

    cors and dbInitialze

--- index.js
+++ index.js
@@ -2,15 +2,40 @@ const express=require("express")
 const app=express()
 
 const sqlite=require("sqlite")
+const {open}=require("sqlite")
 const sqlite3=require("sqlite3")
+const cors=require("cors")
 
 require('dotenv').config();
 const dbPath=process.env.DB_PATH
 
-app.use("/", (request,response)=>{
-    response.json({message: "Hello Agoda CLone Api"})
+app.use(express.json())
+app.use(cors())
+
+let db;
+const initializeDbAndServer= async()=>{
+    try{
+        db= await open({
+            filename: dbPath,
+            driver:sqlite3.Database
+        })
+
+        app.listen(3000, ()=>{
+            console.log("Server Running at 3000")
+        })
+
+    }
+    catch(e){
+        console.log(e.message)
+    }
+}
+
+initializeDbAndServer();
+
+//Write all apis here 
+
+app.get("/hotels", (request,response)=>{
+    response.json("hii")
 })
 
-app.listen(3000, ()=>{
-    console.log("Server Running at 3000")
-})
+
