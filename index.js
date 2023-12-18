0de49d2123eaabacc0bfa040b7bc4984fa2e20bf
mayur <mayursahare91@gmail.com>
2023-12-18 22:35:54 +0530

    dot env

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express=require("express")
 const app=express()
 
+const sqlite=require("sqlite")
+const sqlite3=require("sqlite3")
+
+require('dotenv').config();
+const dbPath=process.env.DB_PATH
+
 app.use("/", (request,response)=>{
     response.json({message: "Hello Agoda CLone Api"})
 })
